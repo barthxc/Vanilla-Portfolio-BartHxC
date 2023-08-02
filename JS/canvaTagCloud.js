432ef76ec42fb8706a4fb21b3818d15d667ead5f
BartHxC <121761009+barthxc@users.noreply.github.com>
2023-08-02 23:18:09 +0200

    canva cloud js

---
+++ JS/canvaTagCloud.js
@@ -0,0 +1,29 @@
+const Texts = [
+    'HTML', 'CSS', 'JAVASCRIPT',
+    'REACT', 'BOOTSTRAP', 'SASS',
+    'PYTHON', 'DJANGO',
+    'JAVA', 'SPRINGBOOT', 'NODEJS',
+    'MYSQL', 'JQUERY', 'PHP'
+];
+
+var tagCloud = TagCloud('.Sphere', Texts, {
+
+    // Sphere radius in px
+    radius: 150,
+
+    // animation speed
+    // slow, normal, fast
+    maxSpeed: 'fast',
+    initSpeed: 'fast',
+
+    // Rolling direction [0 (top) , 90 (left), 135 (right-bottom)] 
+    direction: 135,
+
+    // interaction with mouse or not [Default true (decelerate to rolling init speed, and keep rolling with mouse).]
+    keep: true
+
+});
+
+// Giving color to each text in sphere
+var color = '#36A2EB ';
+document.querySelector('.Sphere').style.color = color;
